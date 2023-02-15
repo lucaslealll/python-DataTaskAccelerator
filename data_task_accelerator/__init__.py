1055f01657a1d54ef8ca3005aa02d48c9ad17ff8
lucasoal <lucasoalzst@gmail.com>
2023-02-15 20:52:29 -0300

    import functions

--- data_task_accelerator/__init__.py
+++ data_task_accelerator/__init__.py
@@ -1,6 +1,6 @@
 """ __init__.py """
 
-from data_task_accelerator.timer import Timer
+from data_task_accelerator.timer import get_time_now, pls_wait
 
 __version__ = "1.0.0"
 __author__ = "Lucas Omar Andrade Leal"
